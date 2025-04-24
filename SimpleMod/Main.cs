4bd4e942c88ae3d5ae324643f224d27f0d50db50
sig649 <sig7alista@gmail.com>
2025-04-24 21:31:58 +0900

    Add files via upload

--- SimpleMod/Main.cs
+++ SimpleMod/Main.cs
@@ -23,36 +23,127 @@ namespace WeightModification
 
             //////-----Config Entry---------------------------------------------------------------------------------- 
             //private static ConfigEntry<bool> CE_AllowFunction01WL;
-            private static ConfigEntry<int> CE_LogLevel;
-            private static ConfigEntry<bool> CE_Rule_BurdenMod;
-            private static ConfigEntry<bool> CE_Rule_LimitLiftingThing;
-            private static ConfigEntry<bool> CE_Rule_ParasiteSupportWhenLifting;
-            private static ConfigEntry<int> CE_Rule_CarryWeightMulti;
+            public static ConfigEntry<int> CE_LogLevel;//デバッグ用のログの出力LV　-1:出力しない 0~:第二引数に応じて出力
+            public static ConfigEntry<bool> CE_Rule_BurdenMod;//R00:重荷状態を改変するかどうか
+            public static ConfigEntry<bool> CE_Rule_LimitLiftingInstalled;//R01:持ち上げられるアイテムの重さを制限するかどうか
+            public static ConfigEntry<bool> CE_Rule_ParasiteSupportWhenLifting;//R01:持ち上げ時に共生相手が手伝ってくれるかどうか
+            public static ConfigEntry<int> CE_Rule_CarryWeightMulti;//R01:持ち上げ可能重量（＝this * WeightLimit）[%]
+            public static ConfigEntry<bool> CE_Rule_ApplyWLMultiForEachRaces;//R02:重量限界を種族ごとに変動させるかどうか
+            //for Rule02-------------------------------------------
+            public static ConfigEntry<string> CE_SizeSS_List;
+            public static ConfigEntry<string> CE_SizeS_List;
+            //private static ConfigEntry<string> CE_SizeM_List;
+            public static ConfigEntry<string> CE_SizeL_List;
+            public static ConfigEntry<string> CE_SizeLL_List;
+            public static ConfigEntry<int> CE_SizeSS_WLMulti;
+            public static ConfigEntry<int> CE_SizeS_WLMulti;
+            public static ConfigEntry<int> CE_SizeM_WLMulti;
+            public static ConfigEntry<int> CE_SizeL_WLMulti;
+            public static ConfigEntry<int> CE_SizeLL_WLMulti;
+            public static ConfigEntry<int> CE_SizeEX_WLMulti;
+            //---------------------------------------------for Rule02
+
+
+
         
             //config--------------------------------------------------------------------------------------------------------------
             //public static bool cf_Allow_F01_WL =>  CE_AllowFunction01WL.Value;
-            public static int cf_LogLevel =>  CE_LogLevel.Value;
-            public static bool cf_Rule00_BurdenMod =>  CE_Rule_BurdenMod.Value;
-
-            public static bool cf_Rule01_LimitLifting =>  CE_Rule_LimitLiftingThing.Value;
-            public static bool cf_Rule01_LiftingSupport =>  CE_Rule_ParasiteSupportWhenLifting.Value;
-            public static int cf_Rule01_CarryWeightMulti
+            internal static int cf_LogLevel =>  CE_LogLevel.Value;
+            //Rule00
+            internal static bool cf_Rule00_BurdenMod =>  CE_Rule_BurdenMod.Value;
+            //Rule01
+            internal static bool cf_Rule01_LimitLifting =>  CE_Rule_LimitLiftingInstalled.Value;
+            internal static bool cf_Rule01_LiftingSupport =>  CE_Rule_ParasiteSupportWhenLifting.Value;
+            internal static int cf_Rule01_CarryWeightMulti
             {
 		        get {return Mathf.Clamp(CE_Rule_CarryWeightMulti.Value,10,10000);}
 	        }
+            //Rule02
+            internal static bool cf_ApplyWLMForEachRaces =>CE_Rule_ApplyWLMultiForEachRaces.Value;
+            private static List<string> cf_SizeSS_List => CE_SizeSS_List.Value.Split(',').Select(s => s.Trim()).ToList();
+            private static List<string> cf_SizeS_List => CE_SizeS_List.Value.Split(',').Select(s => s.Trim()).ToList();
+            private static List<string> cf_SizeL_List => CE_SizeL_List.Value.Split(',').Select(s => s.Trim()).ToList();
+            private static List<string> cf_SizeLL_List => CE_SizeLL_List.Value.Split(',').Select(s => s.Trim()).ToList();
+            private static List<string> cf_SizeEX_List => CE_SizeLL_List.Value.Split(',').Select(s => s.Trim()).ToList();
 
-
-
-            
+            internal static int cf_SizeSS_WLMulti
+            {
+		        get {return Mathf.Clamp(CE_SizeSS_WLMulti.Value,1,10000);}
+	        }
+            internal static int cf_SizeS_WLMulti
+            {
+		        get {return Mathf.Clamp(CE_SizeS_WLMulti.Value,1,10000);}
+	        }
+            internal static int cf_SizeM_WLMulti
+            {
+		        get {return Mathf.Clamp(CE_SizeM_WLMulti.Value,1,10000);}
+	        }
+            internal static int cf_SizeL_WLMulti
+            {
+		        get {return Mathf.Clamp(CE_SizeL_WLMulti.Value,1,10000);}
+	        }
+            internal static int cf_SizeLL_WLMulti
+            {
+		        get {return Mathf.Clamp(CE_SizeLL_WLMulti.Value,1,10000);}
+	        }
+            internal static int cf_SizeEX_WLMulti
+            {
+		        get {return Mathf.Clamp(CE_SizeEX_WLMulti.Value,1,10000);}
+	        }
+            internal static int GetWLMulti(int size)
+            {
+                switch(size)
+                {
+                    case 0 : return cf_SizeSS_WLMulti;
+                    break;
+                    case 1 : return cf_SizeS_WLMulti;
+                    break;
+                    case 2 : return cf_SizeM_WLMulti;
+                    break;
+                    case 3 : return cf_SizeL_WLMulti;
+                    break;
+                    case 4 : return cf_SizeLL_WLMulti;
+                    break;
+                    case 999 : return cf_SizeS_EXMulti;
+                    break;
+                    default : return 100;
+                    break;
+                }
+            }
+            internal static int GetSize(string raceid)
+            {
+                if(cf_SizeEX_List.Contains(raceid)){return 999;}
+                if(cf_SizeLL_List.Contains(raceid)){return 4;}
+                if(cf_SizeL_List.Contains(raceid)){return 3;}
+                //if(cf_SizeM_List.Contains(raceid)){return 2;}
+                if(cf_SizeS_List.Contains(raceid)){return 1;}
+                if(cf_SizeSS_List.Contains(raceid)){return 0;}
+                return 2;
+            }
              //loading----------------------------------------------------------------------------------------------------------
             internal void LoadConfig()
             {
                 //CE_AllowFunction01WL = Config.Bind("#00-General","AllowF01WL", true, "Allow control of function 01-WL");
                 CE_LogLevel = Config.Bind("#zz-Debug","LogLevel", 0, "For debug use. If the value is -1, it won't output logs");
-                CE_Rule_BurdenMod = Config.Bind("#Rule-00", "BurdenCalcMod", true, "Change the calculation of the burden condition.");
-                CE_Rule_LimitLiftingThing = Config.Bind("#Rule-01", "LimitLifting", true, "Limit the weight of installed things that can be lifted.");
-                CE_Rule_ParasiteSupportWhenLifting = Config.Bind("#Rule-01", "LiftingSupport", true, "Parasitic mates help with the lifting.");
-                CE_Rule_CarryWeightMulti = Config.Bind("#Rule-01", "CarryWeightMulti", 500, "Multiplier of weight to be lifted [%]");
+
+                CE_Rule_BurdenMod = Config.Bind("#Rule00", "BurdenCalcMod", true, "Change the calculation of the burden condition.");
+                CE_Rule_LimitLiftingInstalled = Config.Bind("#Rule01", "LimitLifting", true, "Limit the weight of installed things that can be lifted.");
+                CE_Rule_ParasiteSupportWhenLifting = Config.Bind("#Rule01", "LiftingSupport", true, "Parasitic mates help with the lifting.");
+                CE_Rule_CarryWeightMulti = Config.Bind("#Rule01_CarryWeight", "CarryWeightMulti", 1000, "Multiplier of weight to be lifted [%]");
+                CE_Rule_ApplyWLMultiForEachRaces = Config.Bind("#Rule02", "ApplyWLMultiForEachRaces", true, "Apply Weight Limit multiplier for each races.");
+
+                CE_SizeSS_List = Config.Bind("#Rule02_Value-RaceSize", "Size SS List", "fairy,snail,slime,rat,quickling,metal", "[SS]A list of strings separated by commas.");
+                CE_SizeS_List = Config.Bind("#Rule02_Value-RaceSize", "Size S List", "shiba,eldercrab,rabbit,frog,centipede,mandrake,beetle,mushroom,bat,eye,wasp,imp,hand,snake,spider,crab,cat,dog,wisp,chicken,animal", "[S]A list of strings separated by commas.");
+                CE_SizeL_List = Config.Bind("#Rule02_Value-RaceSize", "Size L List", "troll,minotaur,bear,armor,phantom,rock,piece,machine,bike,fish", "[L]A list of strings separated by commas.");
+                CE_SizeLL_List = Config.Bind("#Rule02_Value-RaceSize", "Size LL List", "ent,wyvern,giant,drake,dragon,dinosaur,cerberus", "[LL]A list of strings separated by commas.");
+                CE_SizeEX_List = Config.Bind("#Rule02_Value-RaceSize", "Size EX List", "catgod,machinegod,undeadgod,god", "[Exclusive]A list of strings separated by commas.");
+
+                CE_SizeSS_WLMulti = Config.Bind("#Rule02_Value-WLMulti", "SS WL Multi", 10, "[%]Multiplier of weight limit for races of size SS");
+                CE_SizeS_WLMulti = Config.Bind("#Rule02_Value-WLMulti", "SS WL Multi", 25, "[%]Multiplier of weight limit for races of size S");
+                CE_SizeM_WLMulti = Config.Bind("#Rule02_Value-WLMulti", "SS WL Multi", 50, "[%]Multiplier of weight limit for races of size M");
+                CE_SizeL_WLMulti = Config.Bind("#Rule02_Value-WLMulti", "SS WL Multi", 75, "[%]Multiplier of weight limit for races of size L");
+                CE_SizeLL_WLMulti = Config.Bind("#Rule02_Value-WLMulti", "SS WL Multi", 100, "[%]Multiplier of weight limit for races of size LL");
+                CE_SizeEX_WLMulti = Config.Bind("#Rule02_Value-WLMulti", "SS WL Multi", 200, "[%]Multiplier of weight limit for races of exclusive list");
 
             }
             private void Start()
---
+++ SimpleMod/WeightLimitPatch.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using HarmonyLib;
+
+using UnityEngine;
+using BepInEx.Configuration;
+using System.IO;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using WeightModification.PatchMain;
+
+namespace WeightModification
+{//namespace main
+    namespace WeightLimitPatch
+    {//namespace sub
+        [HarmonyPatch]
+        internal class WLPMain
+        {//class[WLPMain]
+            //entry---------------------------
+            internal class RaceSize
+            {
+                public string id { get; set; }
+                public int value { get; set; }
+                //public int GetValue()
+            }
+            private static bool HasRaceSize(List<RaceSize> sizelist, string c_race)
+            {
+                if(sizelist == null){return false;}
+                foreach(RaceSize rs in sizelist)
+                {
+                    if(rs.id == c_race)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            private static int GetRSValue(List<RaceSize> sizelist, string c_race)
+            {
+                if(sizelist == null){return -999;}
+                foreach(RaceSize rs in sizelist)
+                {
+                    if(rs.id == c_race)
+                    {
+                        return rs.value;
+                    }
+                }
+                return -999;
+            }
+            private static bool Rule_ApplyWLMForEachRaces => Main.cf_ApplyWLMForEachRaces;
+            var SizeList = new List<RaceSize>();
+            
+            //----nakami-------------------
+            [HarmonyPostfix]
+		    [HarmonyPatch(typeof(Chara), "WeightLimit", MethodType.Getter)]
+    		public static void WeightLimit_PostPatch(Chara __instance, ref int __result)
+    		{
+                Chara c = __instance;
+                string c_race = c.race.id;
+                int c_size;
+                //RaceSize rs = new RaceSize();
+                if(!HasRaceSize(SizeList,c_race))
+                {
+                    RaceSize rs = new RaceSize();
+                    rs.id = c_race;
+                    rs.value = Main.GetSize(c_race);
+                    SizeList.add(rs);
+                    c_size = rs.value;
+                } else {
+                    c_size = GetRSValue(SizeList,c_race);
+                }
+    			if (c.IsPC && Rule_ApplyWLMForEachRaces){
+    				//if(IFWMain.HasWeightLimitPenalty(c)){
+    					//float rs = (float)(__result) * IFWMain.configWeightLimitMulti;
+    				__result = __result * Main.GetWLMulti(c_size) / 100;
+    				//}
+	    		}
+		    }
+        }//class[WLPMain]
+    }//namespace sub
+}//namespace main
